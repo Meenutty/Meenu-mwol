f657328de4b95b012a008a0cd1959ad1e4d77bf3
Afx-Abu <100409086+Afx-Abu@users.noreply.github.com>
2022-03-25 07:17:48 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const path = require("path");
 const events = require("./events");
 
 const raganork = require("./raganork");
-const liza = require('./Meenu');
+const Meenu = require('./Meenu');
 const { FakeDB, takeMessage } = require("./plugins/sql/fake");
 const chalk = require('chalk');
 const config = require('./config');
