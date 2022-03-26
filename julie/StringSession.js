10d9ea751c2648179531fa0e22e8af4253e58b6c
Meenutty <101888055+Meenutty@users.noreply.github.com>
2022-03-26 12:15:48 +0530

    Update StringSession.js

--- julie/StringSession.js
+++ julie/StringSession.js
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'LizaMwol:::' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'MEENU:::' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
