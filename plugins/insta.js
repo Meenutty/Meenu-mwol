701b1e123eb66ad0cc0354189a998191c3444460
Jackz-ser <96305513+Jackz-ser@users.noreply.github.com>
2022-03-26 21:09:00 +0530

    Update insta.js

--- plugins/insta.js
+++ plugins/insta.js
@@ -1,15 +1,82 @@
 const Asena = require('../events');
-const { MessageType } = require('@adiwajshing/baileys');
-const axios = require('axios')
-const LOAD_ING = "```Downloading media...```" 
-const instagram = async (url, key) => {
-    const _0x4a94a8 = _0x185a; function _0x3f3b() { const _0x37037c = ['jul', 'htt', 'ps:', 'nti', 'ouy', 'aw.', 'kit', '/in', 'sta', '?ur', 'dat', 'get', 'arr']; _0x3f3b = function () { return _0x37037c; }; return _0x3f3b(); } function _0x185a(_0x38e93d, _0x3f3b83) { const _0x185a5f = _0x3f3b(); _0x185a = function (_0x829ec5, _0x405d60) { _0x829ec5 = _0x829ec5 - 0xe5; let _0x20f676 = _0x185a5f[_0x829ec5]; return _0x20f676; }; return _0x185a(_0x38e93d, _0x3f3b83); } if (key != _0x4a94a8(0xe5) + 'ie') throw new Error('Jul' + 'ie'); const response = await axios(_0x4a94a8(0xe6) + _0x4a94a8(0xe7) + '//u' + _0x4a94a8(0xe8) + 'tle' + 'd-1' + _0x4a94a8(0xe9) + 'r1r' + 'szh' + _0x4a94a8(0xea) + 'run' + _0x4a94a8(0xeb) + '.sh' + _0x4a94a8(0xec) + _0x4a94a8(0xed) + _0x4a94a8(0xee) + 'l=' + url); const { status, result } = response[_0x4a94a8(0xef) + 'a']; if (!status) return { 'status': status }; const { type, data } = result[0x0]; const res = await axios[_0x4a94a8(0xf0)](data, { 'responseType': _0x4a94a8(0xf1) + 'ayb' + 'uff' + 'er' }); return { 'type': type, 'data': res[_0x4a94a8(0xef) + 'a'], 'status': status };
-}
+const {MessageType} = require('@adiwajshing/baileys');
+const got = require('got');
+const fs = require('fs');
+const axios = require('axios');
+const Config = require('../config');
+const { errorMessage, infoMessage } = require('../helpers');
+const IG_DESC = "Downloads Image/Video From Instagram"
+const NEED_WORD = "Must Enter a link"
+const FBDESC = "Downloads Video From FaceBook"
+const LOADING = "Downloading the Video..."
+const NOT_FOUNDFB = "Video Not Found"
+const CAPTION = "Caption"
 
-Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false, desc: "Downloads from instagaram", dontAddCommandList: true }, async (message, match) => { 
-    const { status, type, data } = await instagram(match[1], 'julie')
-    if (!status) return await message.sendMessage('not found')
-    await message.client.sendMessage(message.jid, LOAD_ING, MessageType.text, { quoted: message.data });
-    if (type === 'image') return await message.sendMessage(data, MessageType.image, { caption: "```Made By Liza Mwol```", quoted: message.data })
-    if (type === 'video') return await message.sendMessage(data, MessageType.video, { caption: "```Made By Liza Mwol```", quoted: message.data })
-});
+Asena.addCommand({ pattern: 'ig ?(.*)', fromMe: false, desc: IG_DESC}, async (message, match) => {
+
+    const userName = match[1]
+
+    if (!userName) return await message.sendMessage(errorMessage(NEED_WORD))
+
+    await message.sendMessage(infoMessage("Downloading the Post..."))
+
+    await axios
+      .get(`https://api-anoncybfakeplayer.herokuapp.com/igdown?url=${userName}`)
+      .then(async (response) => {
+        const {
+          url,
+          type,
+        } = response.data.result[0]
+
+        const profileBuffer = await axios.get(url, {responseType: 'arraybuffer'})
+
+        const msg = `${type}`
+
+	 if (msg === 'image') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
+          caption: +Config.ALL+
+        })}
+		 	 
+	if (msg === 'video') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
+          caption: +Config.ALL+
+        })}
+	
+        
+      })
+      .catch(
+        async (err) => await message.sendMessage(errorMessage("Invaild Link, Please Enter a Vaild Instagram Link")),
+      )
+  },
+)
+
+
+
+
+Asena.addCommand({ pattern: 'fb ?(.*)', fromMe: false, desc: FBDESC }, async (message, match) => {
+
+    const userName = match[1]
+
+    if (!userName) return await message.sendMessage(errorMessage(NEED_WORD))
+
+    await message.sendMessage(infoMessage(LOADING))
+
+    await axios
+      .get(`https://videfikri.com/api/fbdl/?urlfb=${userName}`)
+      .then(async (response) => {
+        const {
+          url,
+          judul,
+        } = response.data.result
+
+        const profileBuffer = await axios.get(url, {responseType: 'arraybuffer'})
+
+        const msg = `*${CAPTION}*: ${judul}`
+
+        await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
+          caption: +Config.ALL+
+        })
+      })
+      .catch(
+        async (err) => await message.sendMessage(errorMessage(NOT_FOUNDFB )),
+      )
+  },
+)
