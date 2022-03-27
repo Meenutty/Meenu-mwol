a07a04e8106b90511445fab6fa2d9fb4945813eb
Jackz-ser <96305513+Jackz-ser@users.noreply.github.com>
2022-03-27 13:19:33 +0530

    Update xxxtentacion.js

--- plugins/xxxtentacion.js
+++ plugins/xxxtentacion.js
@@ -55,7 +55,7 @@ var i = Math.floor(15*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Mᴀᴅᴇ Bʏ ᴍᴇᴇɴᴜ ᴍᴡᴏʟ'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: +Config.ALL+})
 
     }));
 
@@ -104,7 +104,7 @@ r_text[15] = "https://telegra.ph/file/00a4732edf86b425ecdea.jpg";
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Mᴀᴅᴇ Bʏ ᴍᴇᴇɴᴜ ᴍᴡᴏʟ'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: +Config.ALL+})
 
     }));
 
