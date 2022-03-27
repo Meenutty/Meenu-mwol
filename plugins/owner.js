b4855fb277316d898240019467b3a7fa16a94a3c
Jackz-ser <96305513+Jackz-ser@users.noreply.github.com>
2022-03-27 07:13:21 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -12,7 +12,7 @@ if (Config.WORKTYPE == 'private') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*LIZA-MWOL by Fayas and Nithin*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'_*MEENU MWOL BY JACKZ,ABU,AMRU,FOXY*_' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n---------------------', MessageType.text);
@@ -30,7 +30,7 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*LIZA-MWOL by Fayas and Nithin*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'_*MEENU MWOL BY JACKZ,ABU,AMRU,FOXY*_' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n--------------------', MessageType.text);
