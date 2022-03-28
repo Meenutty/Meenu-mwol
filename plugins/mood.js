677af1b0f9404f756c78a2ed25fecf66ec5cd067
Jackz-ser <96305513+Jackz-ser@users.noreply.github.com>
2022-03-28 16:16:52 +0530

    Update mood.js

--- plugins/mood.js
+++ plugins/mood.js
@@ -91,6 +91,6 @@ await message.sendMessage(`According To My Calculations🧐\nYou Are Now ${r_tex
 
 Asena.addCommand({pattern: 'infomood', fromMe: true}, (async (message, match) => {
     
-    await message.sendMessage('*Codded by Nithin@Fayas*\n💻Usage: *.mood <reply>*\nℹ️Desc: Finds what your responding one‘s moods.\n🆕 _Support Filters!_');
+    await message.sendMessage('*Codded by Jackz*\n💻Usage: *.mood <reply>*\nℹ️Desc: Finds what your responding one‘s mood.\n🆕 _Support Filters!_');
 
 }));
