729a2e7d9d9061a49ecf426001ff4b7e1c340271
Jackz-ser <96305513+Jackz-ser@users.noreply.github.com>
2022-03-29 12:13:10 +0530

    Update insta.js

--- plugins/insta.js
+++ plugins/insta.js
@@ -1,81 +1,28 @@
-const Asena = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
-const got = require('got');
-const fs = require('fs');
-const axios = require('axios');
+const { MessageType, Mimetype } = require('@adiwajshing/baileys');
+const MyPnky = require('../events');
+const Config = require('../config');
+const { igDownloader, igstalk } = require('../scraper.js')
 const { errorMessage, infoMessage } = require('../helpers');
-const IG_DESC = "Downloads Image/Video From Instagram"
-const NEED_WORD = "Must Enter a link"
-const FBDESC = "Downloads Video From FaceBook"
-const LOADING = "Downloading the Video..."
-const NOT_FOUNDFB = "Video Not Found"
-const CAPTION = "Caption"
+const axios = require('axios');
+const fs = require('fs');
+const gis = require('g-i-s');
+const got = require("got");
 
-Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false, desc: IG_DESC}, async (message, match) => {
-
-    const userName = match[1]
-
-    if (!userName) return await message.sendMessage(errorMessage(NEED_WORD))
-
-    await message.sendMessage(infoMessage("Downloading the Post..."))
-
-    await axios
-      .get(`https://api-anoncybfakeplayer.herokuapp.com/igdown?url=${userName}`)
-      .then(async (response) => {
-        const {
-          url,
-          type,
-        } = response.data.result[0]
-
-        const profileBuffer = await axios.get(url, {responseType: 'arraybuffer'})
-
-        const msg = `${type}`
-
-	 if (msg === 'image') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
-          caption: 'insta'
-        })}
-		 	 
-	if (msg === 'video') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
-          caption: 'insta'
-        })}
-	
-        
-      })
-      .catch(
-        async (err) => await message.sendMessage(errorMessage("Invaild Link, Please Enter a Vaild Instagram Link")),
-      )
-  },
-)
+let wk = Config.WORKTYPE == 'public' ? false : true
 
 
+MyPnky.addCommand({pattern: 'insta ?(.*)', fromMe: wk, desc: "download from Instagram"}, async (message, match) => {
+  try{
+    if (!match[1]) return await message.sendMessage("enter a link");
+    await message.sendMessage("_*DOWNLOADING...*_")
 
-
-Asena.addCommand({ pattern: 'fb ?(.*)', fromMe: false, desc: FBDESC }, async (message, match) => {
-
-    const userName = match[1]
-
-    if (!userName) return await message.sendMessage(errorMessage(NEED_WORD))
-
-    await message.sendMessage(infoMessage(LOADING))
-
-    await axios
-      .get(`https://videfikri.com/api/fbdl/?urlfb=${userName}`)
-      .then(async (response) => {
-        const {
-          url,
-          judul,
-        } = response.data.result
-
-        const profileBuffer = await axios.get(url, {responseType: 'arraybuffer'})
-
-        const msg = `*${CAPTION}*: ${judul}`
-
-        await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
-          caption: 'insta'
-        })
-      })
-      .catch(
-        async (err) => await message.sendMessage(errorMessage(NOT_FOUNDFB )),
-      )
-  },
-)
+    res = await igDownloader(match[1])
+    ytm = res.result
+    const profileBuffer = await axios.get(`${ytm.link}`, {responseType: 'arraybuffer'})
+    const msg = `${ytm.link}`
+     if (msg.includes('.mp4')) { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, { caption: `video`, quoted: message.data })}
+     if (msg.includes('.jpg')) { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, { caption: `image`, quoted: message.data })}
+  } catch {
+     await message.sendMessage("error")
+  }
+});
