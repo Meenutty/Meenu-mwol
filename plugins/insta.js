d88557fcb0fc8d189a0edbb9775a80a368cac05c
Jackz-ser <96305513+Jackz-ser@users.noreply.github.com>
2022-03-29 13:21:05 +0530

    Update insta.js

--- plugins/insta.js
+++ plugins/insta.js
@@ -1,7 +1,7 @@
 const { MessageType, Mimetype } = require('@adiwajshing/baileys');
 const MyPnky = require('../events');
 const Config = require('../config');
-const { igDownloader, igstalk } = require('../scraper.js')
+const { igDownloader, igstalk } = require('../media/scraper.js')
 const { errorMessage, infoMessage } = require('../helpers');
 const axios = require('axios');
 const fs = require('fs');
@@ -13,15 +13,15 @@ let wk = Config.WORKTYPE == 'public' ? false : true
 
 MyPnky.addCommand({pattern: 'insta ?(.*)', fromMe: wk, desc: "download from Instagram"}, async (message, match) => {
   try{
-    if (!match[1]) return await message.sendMessage("enter a link");
-    await message.sendMessage("_*DOWNLOADING..*_")
+    if (!match[1]) return await message.sendMessage("_Please enter a valid link_");
+    await message.sendMessage("_*DOWNLOADING*_")
 
     res = await igDownloader(match[1])
     ytm = res.result
     const profileBuffer = await axios.get(`${ytm.link}`, {responseType: 'arraybuffer'})
     const msg = `${ytm.link}`
-     if (msg.includes('.mp4')) { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, { caption: `video`, quoted: message.data })}
-     if (msg.includes('.jpg')) { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, { caption: `image`, quoted: message.data })}
+     if (msg.includes('.mp4')) { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, { caption: `Instagram•Video`, quoted: message.data })}
+     if (msg.includes('.jpg')) { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, { caption: `Instagram•Image`, quoted: message.data })}
   } catch {
      await message.sendMessage("error")
   }
