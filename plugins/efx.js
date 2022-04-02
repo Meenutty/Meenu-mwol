7795076b2af9aad17b53d1526e6be2091026fc25
Jackz-ser <96305513+Jackz-ser@users.noreply.github.com>
2022-04-02 06:34:36 +0530

    all efx commnds

--- plugins/efx.js
+++ plugins/efx.js
@@ -1,1 +1,49 @@
-//efx 
+
+const Jackz= require('../events');
+
+const Config = require('../config');
+
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+
+const axios = require('axios');
+
+Jackz.addCommand({pattern: 'efx', fromMe: false, desc: 'It sends all efx list'}, (async (message, match) => {
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.jpg, contextInfo: {mentionedJid: message.mention}, caption: `╭──────────────────╮ 
+╭──────────────────╯
+│ EFX AUDIO NAME LIST
+╰──────────────────╮
+                                                       
+  ❏ Bᴏᴛ ɴᴀᴍᴇ : `+config.BOTV2+`   
+                                                       
+╭──────────────────╯
+│⚠︎ use  .efx(number)
+│
+│efx1 = Infinity audio
+│efx2 = love again audio
+│efx3 = Jack Sparrow 
+│efx4 = Infected 
+│efx5 = I m beggin
+│efx6 = Wasted neoo p
+│efx7 = Infinity audio 2      
+│efx8 = enakkage poranthaye tamil
+│efx9 = Devil eyes    
+│efx10 = Runnin low  
+│efx11 = Arcadia (skeler) 
+│efx12 = No idea
+│efx13 = Go down deh
+│efx14 = Kun faya kun
+│efx15 = I love you baby
+╰───────────────────╮
+╭───────────────────╯
+│_*CREATED BY JACKZ*_
+│_*CONCEPT BY AMRU*_
+│©
+╰───────────────────╮
+╰───────────────────╯ 
+
+`}) 
+
+}));                                                                 
+                                                                              
+                                                                                                                                                        
