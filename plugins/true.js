09efd4ee4a032983ab184700993e012b44c6e7af
Sourav K <87891508+souravkl11@users.noreply.github.com>
2022-04-03 10:46:43 +0530

    Poyi padik mwonu

--- plugins/true.js
+++ plugins/true.js
@@ -17,8 +17,6 @@ go = mm
 } 
 var initt = go.split(" ").join("")
 var number = initt.replace('+','')
-var number = query || msg.reply_message.jid
 var {data} = await axios(`https://raganork-api.herokuapp.com/api/truecaller?text=in&username=${number}&apikey=souravkl11`);
-var result = data.result.data[0] == 'error' ? 'Limit over (50/50). Wait for tomorrow' : data.result.data[0]
-await msg.sendMessage(result)
+await msg.sendMessage(data.result.data[0] == 'error' ? 'Server limit over (50/50)' : data.result.data[0])
 })
