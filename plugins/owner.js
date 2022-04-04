3fa2072453548335e1d814a305e2acb58e4fc4d9
Jackz-ser <96305513+Jackz-ser@users.noreply.github.com>
2022-04-04 16:45:15 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -13,7 +13,6 @@ var clh = { cd: 'L3Jvb3QvV2hhdHNBc2VuYUR1cGxpY2F0ZWQv', pay: '' }
 var ggg = Buffer.from(clh.cd, 'base64')
 var ddd = ggg.toString('utf-8')
 const url1 = Config.LG_LOGO;
-const buff1 = await getBuffer(url1)
 
 
 
@@ -21,6 +20,8 @@ let whb = Config.WORKTYPE == 'public' ? false : true
 
 Jackz.addCommand({pattern: 'owner', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
 // send a buttons message!credit Jackz
+    async (message, match) => {
+        const buff1 = await.getBuffer(url1)
     const buttons = [
         {buttonId: 'id1', buttonText: {displayText: Config.OWNER }, type: 1},
       ]
@@ -34,4 +35,4 @@ Jackz.addCommand({pattern: 'owner', fromMe: whb, dontAddCommandList: true}, (asy
       
 await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage, { mimetype: Mimetype.buttonsMessage, quoted: message.data, ptt: true,quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { orderMessage: { itemCount: 123, status: 200, thumbnail: fs.readFileSync ('buff1.buffer'), surface: 200, message: Config.BOTV2, orderTitle: Config.BOTV2, "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Config.BOTV2 + '\n', "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1080, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('buff1.buffer')}}}});
 
-}));
+}}));
