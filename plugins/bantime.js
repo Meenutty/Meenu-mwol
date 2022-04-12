2315b93333111595a993223bf312f1d24c93c9c1
Jackz-ser <96305513+Jackz-ser@users.noreply.github.com>
2022-04-12 13:22:41 +0530

    Create bantime.js

---
+++ plugins/bantime.js
@@ -0,0 +1,75 @@
+
+const {MessageType, GroupSettingChange} = require('@adiwajshing/baileys'); 
+const Asena = require('../events');
+const Config = require('../config');
+
+const Language = require('../language');
+const Lang = Language.getString('admin');
+const fs = require('fs');
+
+
+
+async function checkImAdmin(message, user = message.client.user.jid) {
+    var grup = await message.client.groupMetadata(message.jid);
+    var sonuc = grup['participants'].map((member) => {
+        
+        if (member.jid.split("@")[0] == user.split("@")[0] && member.isAdmin) return true; else; return false;
+    });
+    return sonuc.includes(true);
+}
+
+Asena.addCommand({pattern: 'ban ?(.*)', fromMe: true, dontAddCommandList: true, desc: Lang.BAN_DESC}, (async (message, match) => {    
+    
+    if (message.jid.endsWith('@g.us')) {
+    var im = await checkImAdmin(message);
+    if (!im) return await message.client.sendMessage(message.jid,Lang.IM_NOT_ADMIN,MessageType.text);
+    
+        else if (match[1] == '1') {
+            await message.client.groupRemove(message.jid,[message.reply=message.data.participant]);
+            await message.client.sendMessage(message.jid,mut.BİRMUTE,MessageType.text);
+
+            await new Promise(r => setTimeout(r, 60000));
+    
+            await message.client.groupRemove(message.jid,[message.reply=message.data.participant]);
+            await message.client.sendMessage(message.jid,Lang.UNMUTED,MessageType.text);
+        }
+      (async (message, match) => {  
+   
+    if (message.jid.endsWith('@g.us')) {
+    var im = await checkImAdmin(message);
+    if (!im) return await message.client.sendMessage(message.jid,Lang.IM_NOT_ADMIN,MessageType.text);
+
+    if (Config.BANMSG == 'default') {
+        if (message.reply_message !== false) {
+            await message.client.sendMessage(message.jid,'@' + message.reply_message.data.participant.split('@')[0] + '```, ' + Lang.BANNED + '```', MessageType.text, {contextInfo: {mentionedJid: [message.reply_message.data.participant]}});
+            await message.client.groupRemove(message.jid, [message.reply_message.data.participant]);
+        } else if (message.reply_message === false && message.mention !== false) {
+            var etiketler = '';
+            message.mention.map(async (user) => {
+                etiketler += '@' + user.split('@')[0] + ',';
+            });
+
+            await message.client.sendMessage(message.jid,etiketler + '```, ' + Lang.BANNED + '```', MessageType.text, {contextInfo: {mentionedJid: message.mention}});
+            await message.client.groupRemove(message.jid, message.mention);
+        } else {
+            return await message.client.sendMessage(message.jid,Lang.GIVE_ME_USER,MessageType.text);
+        }
+    }
+    else {
+        if (message.reply_message !== false) {
+            await message.client.sendMessage(message.jid,'@' + message.reply_message.data.participant.split('@')[0] + Config.BANMSG, MessageType.text, {contextInfo: {mentionedJid: [message.reply_message.data.participant]}});
+            await message.client.groupRemove(message.jid, [message.reply_message.data.participant]);
+        } else if (message.reply_message === false && message.mention !== false) {
+            var etiketler = '';
+            message.mention.map(async (user) => {
+                etiketler += '@' + user.split('@')[0] + ',';
+            });
+
+            await message.client.sendMessage(message.jid,etiketler + Config.BANMSG, MessageType.text, {contextInfo: {mentionedJid: message.mention}});
+            await message.client.groupRemove(message.jid, message.mention);
+        } else {
+            return await message.client.sendMessage(message.jid,Lang.GIVE_ME_USER,MessageType.text);
+        }
+    }
+  }      
+}));
